0ee8f322770d6942b152e6d82bd5e2841ea12afc
Seth Battis <sbattis@groton.org>
2023-03-22 15:25:46 +0000

    Initial Commit

---
+++ src/google-analytics.js
@@ -0,0 +1,14 @@
+// Google analytics tag
+const GA4ID = 'G-LW51J2WF8M';
+((id) => {
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_G4_ID}`;
+  document.getElementsByTagName('head')[0].appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', id);
+})(GA4ID);
