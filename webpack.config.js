65c3fa5e2f9f6d93e496969dade7221b66b5e999
Seth Battis <sbattis@groton.org>
2023-05-16 18:15:07 +0000

    Push terser config into webpack package

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,5 @@ const config = require('@battis/webpack/ts/vanilla');
 
 module.exports = config({
   root: __dirname,
-  bundle: 'kb',
-  terserOptions: {
-    terserOptions: {
-      mangle: { properties: true }
-    }
-  }
+  bundle: 'kb'
 });
