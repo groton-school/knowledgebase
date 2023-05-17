371bb5fff589068e9f37c2d8daa1c90b848ebaea
Seth Battis <sbattis@groton.org>
2023-05-17 00:51:29 +0000

    readable width

--- src/UI/index.ts
+++ src/UI/index.ts
@@ -3,6 +3,7 @@ import './hide-topnav.scss';
 import HighlightAnchor from './HighlightAnchor';
 import './less-stripey-tables.scss';
 import Permalinks from './Permalinks';
+import './readable-width.scss';
 import './space-after-paragraph.scss';
 import TOC from './TOC';
 
