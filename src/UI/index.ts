58b0c2995681862e124b5f50c87a6bbf0f80fe54
Seth Battis <sbattis@groton.org>
2023-05-19 13:34:44 -0400

    unhiding topnav to see if there’s a more elegant solution

--- src/UI/index.ts
+++ src/UI/index.ts
@@ -1,6 +1,6 @@
 import Helper from '../Helper';
 import './fixed-background.scss';
-import './hide-topnav.scss';
+//import './hide-topnav.scss';
 import HighlightAnchor from './HighlightAnchor';
 import Permalinks from './Permalinks';
 import './readable-width.scss';
