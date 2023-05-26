ddb696ec9f9eaed503dbf7c89e34c3da89c9269b
Seth Battis <sbattis@groton.org>
2023-05-25 21:30:23 -0400

    reorder remove/embed firing
    the embed will be removed before it is embedded again, need to fire in that order.

--- src/Helper/onLoad.ts
+++ src/Helper/onLoad.ts
@@ -4,31 +4,42 @@ export const GoogleDocRemoveEvent = 'GoogleDocRemove';
 function observe() {
   const observer = new MutationObserver((mutations) => {
     let embed = undefined;
-    let remove = undefined;
     mutations.forEach((mutation) => {
-      mutation.addedNodes.forEach((node) => {
-        const elt = node as HTMLElement;
-        if (elt.id === 'od-doc-document-first') {
-          embed = elt.querySelector('.CMSgoogledocembed');
-        }
-      });
-      mutation.removedNodes.forEach((node) => {
-        const elt = node as HTMLElement;
-        if (elt.id === 'od-doc-document-first') {
-          remove = elt.querySelector('.CMSgoogledocembed');
-        }
-      });
+      if (
+        Array.from(mutation.removedNodes).reduce((removed, node) => {
+          if (!removed) {
+            const elt = node as HTMLElement;
+            if (
+              elt.id === 'od-doc-document-first' ||
+              elt.id === 'od-grid-stack'
+            ) {
+              return true;
+            }
+          }
+          return removed;
+        }, false)
+      ) {
+        document.body.dispatchEvent(new Event(GoogleDocRemoveEvent));
+      }
+      if (
+        (embed = Array.from(mutation.addedNodes).reduce(
+          (embedded: Element | null, node) => {
+            if (!embedded) {
+              const elt = node as HTMLElement;
+              if (elt.id === 'od-doc-document-first') {
+                return elt.querySelector('.CMSgoogledocembed');
+              }
+            }
+            return embedded;
+          },
+          null
+        ))
+      ) {
+        document.body.dispatchEvent(
+          new CustomEvent(GoogleDocEmbedEvent, { detail: embed })
+        );
+      }
     });
-    if (embed) {
-      document.body.dispatchEvent(
-        new CustomEvent(GoogleDocEmbedEvent, { detail: embed })
-      );
-    }
-    if (remove) {
-      document.body.dispatchEvent(
-        new CustomEvent(GoogleDocRemoveEvent, { detail: remove })
-      );
-    }
   });
   observer.observe(document.body, { childList: true, subtree: true });
 }
