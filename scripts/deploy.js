f678606a5c4de20d610c1896accea4fdedce37cd
Seth Battis <sbattis@groton.org>
2023-05-27 14:59:10 -0400

    sigh, caches

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -60,12 +60,12 @@ async function uploadDirectory() {
     `${successfulUploads} files uploaded to ${bucketName} successfully.`
   );
   console.log(
-    `\n\nPaste the following line into Site Settings/Developers/JS:\n\nrequirejs(['https://${path.join(
+    `\n\nPaste the following line into Site Settings/Developers/JS:\n\nrequirejs([\`https://${path.join(
       'storage.googleapis.com',
       bucketName,
       path.relative(process.cwd(), directoryPath),
       `kb-${pkg.version}.js`
-    )}']);\n\n`
+    )}\`]);\n\n(Hint: you can bypass caching by appending \`?cache=\${crypto.randomUUID()}\` to the URL.)\n\n`
   );
 }
 
