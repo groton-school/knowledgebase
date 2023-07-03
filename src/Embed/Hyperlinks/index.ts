f852a7b73592cb5996fd2de349e8ceb975e3df2c
Seth Battis <sbattis@groton.org>
2023-07-03 10:32:26 -0400

    embed Vimeo hyperlinks

--- src/Embed/Hyperlinks/index.ts
+++ src/Embed/Hyperlinks/index.ts
@@ -1,9 +1,11 @@
 import AppStore from './AppStore';
 import Swank from './Swank';
+import Vimeo from './Vimeo';
 import YouTube from './YouTube';
 
 export default function Hyperlinks() {
   AppStore();
   Swank();
+  Vimeo();
   YouTube();
 }
