07d5f81bd23dc4246084415e1fb3a7db1ae6f4df
Seth Battis <sbattis@groton.org>
2023-11-08 12:55:53 -0500

    Alt text as captions

--- src/UI/Embiggen/index.ts
+++ src/UI/Embiggen/index.ts
@@ -6,20 +6,35 @@ import './styles.scss';
 import styles from './styles.module.scss';
 
 function Embiggenate(e: MouseEvent) {
+  const target = e.target as HTMLImageElement;
+
   const scrim = document.createElement('div');
   scrim.classList.add(styles.embiggen, styles.scrim);
+
   const closeBox = document.createElement('div');
   closeBox.classList.add(styles.closeBox);
   closeBox.innerText = '×';
+  scrim.appendChild(closeBox);
+
+  const wrapper = document.createElement('div');
+  wrapper.classList.add(styles.wrapper);
+  scrim.appendChild(wrapper);
+
   const image = document.createElement('div');
   image.classList.add(styles.image, styles.zoomed);
-  image.style.backgroundImage = `url(${(e.target as HTMLImageElement).src})`;
+  image.style.backgroundImage = `url(${target.src})`;
+  wrapper.appendChild(image);
 
-  scrim.appendChild(closeBox);
-  scrim.appendChild(image);
+  if (target.alt) {
+    const caption = document.createElement('p');
+    caption.classList.add(styles.caption);
+    caption.innerText = target.alt;
+    wrapper.appendChild(caption);
+  }
+
   document.body.appendChild(scrim);
   scrim.addEventListener('click', () => Disembiggenate(scrim));
-  Helper.log(`Embiggening ${(e.target as HTMLImageElement).src}`);
+  Helper.log(`Embiggening ${target.alt || target.src}`);
 }
 
 function Disembiggenate(scrim: HTMLDivElement) {
@@ -34,7 +49,7 @@ export default function Embiggen() {
       images.forEach((image) => {
         image.classList.add(styles.embiggen, styles.image, styles.original);
         image.addEventListener('click', Embiggenate);
-        Helper.log(`${image.src} embiggenable`);
+        Helper.log(`🖼️ ${image.alt || image.src} embiggenable`);
       });
     }
   );
