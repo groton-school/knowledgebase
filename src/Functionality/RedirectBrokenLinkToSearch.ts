80813e78a7c22f9cb44285e1e6770402bf32ba62
Seth Battis <sbattis@groton.org>
2023-11-09 14:11:25 -0500

    At least dumping new users _somewhere_

--- src/Functionality/RedirectBrokenLinkToSearch.ts
+++
@@ -1,7 +0,0 @@
-export default function RedirectBrokenLinkToSearch() {
-  if (document.querySelector('#od-messagepage-alert')) {
-    window.location.href = `${window.location.protocol}//${window.location.hostname
-      }/search?q=${window.location.pathname.replace(/[^a-z0-9]+/gi, '%20')}#${window.location.hash
-      }`;
-  }
-}
