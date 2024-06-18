c52d28f86b8833462ca5aaa7368bd1d568fa9467
Seth Battis <sbattis@groton.org>
2024-06-18 15:11:22 -0400

    simplify setup

--- packages/www/bin/setup.sh
+++ packages/www/bin/setup.sh
@@ -7,29 +7,18 @@ gcloud billing projects link $PROJECT --billing-account=$BILLING --format=json
 
 gcloud services enable docs.googleapis.com --project=$PROJECT --format=json
 gcloud services enable drive.googleapis.com --project=$PROJECT --format=json
+gcloud services enable storage-api.googleapis.com --project=$PROJECT --format=json
 
-# https://cloud.google.com/storage/docs/hosting-static-website#storage-create-bucket-cli
-gcloud services enable compute.googleapis.com --project=$PROJECT --format=json
 gcloud projects add-iam-policy-binding $PROJECT --member=$MEMBER --role=roles/storage.admin --format=json
 gcloud projects add-iam-policy-binding $PROJECT --member=$MEMBER --role=roles/compute.networkAdmin --format=json
 gcloud storage buckets create gs://$BUCKET --location=$LOCATION --format=json --project=$PROJECT
 gcloud storage buckets update gs://$BUCKET --project=$PROJECT --public-access-prevention --no-uniform-bucket-level-access --web-main-page-suffix=index.html --web-error-page=static/404.html
 
-# ...upload some files...
-gcloud storage cp static/404.html gs://$BUCKET/static/404.html --project=$PROJECT --format=json
-
-gcloud storage buckets add-iam-policy-binding  gs://$BUCKET --member=allUsers --role=roles/storage.objectViewer --project=$PROJECT --format=json
-gcloud compute addresses create kb-ip --network-tier=PREMIUM --ip-version=IPV4 --global --project=$PROJECT --format=json
-gcloud compute backend-buckets create $LB --gcs-bucket-name=$BUCKET --project=$PROJECT --format=json
-gcloud compute url-maps create http-lb --default-backend-bucket=$LB --project=$PROJECT --format=json
-gcloud compute target-http-proxies create http-lb-proxy --url-map=http-lb --project=$PROJECT --format=json
-gcloud compute forwarding-rules create http-lb-forwarding-rule --load-balancing-scheme=EXTERNAL --network-tier=PREMIUM --address=kb-ip --global --target-http-proxy=http-lb-proxy --ports=80 --project=$PROJECT --format=json
-
-gcloud services enable storage-api.googleapis.com --project=$PROJECT --format=json
 gcloud app create --region=$LOCATION --project=$PROJECT --format=json
+GAE="$(gcloud app describe --project=$PROJECT --format=json | jq -r '.defaultHostname')"
 
 mkdir -p "$(dirname $0)/../var"
 echo "{\"storage\":{\"bucket\":\"$BUCKET\"}}" > "$(dirname $0)/../var/config.json"
 
 echo "You need to manually configure the OAuth Consent screen at https://console.cloud.google.com/apis/credentials/consent?project=$PROJECT"
-echo "You need to manually create and download keys for the app screen at https://console.cloud.google.com/apis/credentials?project=$PROJECT (OAuth 2.0, web, set both JavaScript origin and redirect URL to GAE instance"
+echo "You need to manually create and download keys for the app screen at https://console.cloud.google.com/apis/credentials?project=$PROJECT (OAuth 2.0, web, set both JavaScript origin to https://$GAE and redirect URL to https://$GAE/oauth2callback"
