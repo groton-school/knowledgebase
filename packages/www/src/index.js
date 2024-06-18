235f79d61ce4b651be75e87cdf05d83437b736e4
Seth Battis <sbattis@groton.org>
2024-06-17 21:50:52 -0400

    it really _should_ work

--- packages/www/src/index.js
+++ packages/www/src/index.js
@@ -1,49 +1,48 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const google = require('google-auth-library');
+const { OAuth2Client } = require('google-auth-library');
 
 const credentials = require('../credentials.json');
 
-(async () => {
-  const redirectURI = credentials.web.redirect_uris[0];
-  const client = new google.OAuth2Client(
-    credentials.web.client_id,
-    credentials.web.client_secret,
-    redirectURI
-  );
+const redirectURI = new URL(credentials.web.redirect_uris[0]);
+const oauth2 = new OAuth2Client(
+  credentials.web.client_id,
+  credentials.web.client_secret,
+  redirectURI.href
+);
 
-  const app = express();
-  app.use(cookieParser());
+const app = express();
+app.use(cookieParser());
 
-  app.get('/logout', (_, res) => {
-    res.clearCookie('token');
-    res.send('logged out');
+app.get('/logout', (_, res) => {
+  res.clearCookie('token');
+  res.send('logged out');
+  res.end();
+});
+app.get(redirectURI.pathname, async (req, res) => {
+  const redirect = req.cookies?.redirect || '/';
+  res.clearCookie('redirect');
+  res.cookie('token', await oauth2.getToken(req.cookies.code));
+  res.redirect(redirect);
+  res.end();
+});
+app.get('/*', (req, res) => {
+  if (req.cookies?.token) {
+    res.send(`You requested ${req.url}`);
     res.end();
-  });
-  app.get(redirectURI, async (req, res) => {
-    const redirect = req.cookies?.redirect || '/';
-    res.clearCookie('redirect');
-    res.cookie('token', await client.getToken(req.cookies.code), {
-      secure: true
-    });
-    res.redirect(redirect);
+  } else {
+    res.cookie('redirect', req.url);
+    res.redirect(
+      oauth2.generateAuthUrl({
+        access_type: 'offline',
+        scope: 'https://www.googleapis.com/auth/devstorage.read_only'
+      })
+    );
     res.end();
-  });
-  app.get('/*', (req, res) => {
-    if (req.cookies?.token) {
-      res.send(`You requested ${req.url}`);
-      res.end();
-    } else {
-      res.cookie('redirect', req.url, { secure: true });
-      res.redirect(
-        client.generateAuthUrl({
-          access_type: 'offline',
-          scope: 'https://www.googleapis.com/auth/devstorage.read_only'
-        })
-      );
-      res.end();
-    }
-  });
+  }
+});
 
-  app.listen(process.env.PORT || 8080);
-})();
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
