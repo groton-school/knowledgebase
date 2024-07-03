08df2be0a5e599d92f343464ea55a952a5f848c0
Seth Battis <sbattis@groton.org>
2024-07-03 16:07:14 -0400

    generate group IDs

---
+++ packages/builder/bin/groups.ts
@@ -0,0 +1,64 @@
+#!/usr/bin/env tsx
+import cli from '@battis/qui-cli';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const FOLDER_NAME = '%FOLDER_NAME%';
+const TIMESTAMP = '%TIMESTAMP%';
+
+const defaultFilePath = path.resolve(__dirname, '../../server/var/groups.json');
+
+const options = {
+  filePath: {
+    short: 'f',
+    description: `Output JSON file path. If the file already exists, the list will be updated. (defaults to ${cli.colors.url(
+      defaultFilePath
+    )})`,
+    default: defaultFilePath
+  },
+  permissionsRegex: {
+    description: `Regular expression to email addresses of users/groups to include in Cloud Storage Bucket (will be read from ${cli.colors.value(
+      'PERMISSIONS_REGEX'
+    )} environment variable if present)`
+  }
+};
+
+(async () => {
+  const CWD = process.cwd();
+  let {
+    values: { filePath, permissionsRegex }
+  } = await cli.init({ args: { options } });
+
+  const spinner = cli.spinner();
+
+  let groups = [];
+  let nextPageToken;
+  do {
+    const page = JSON.parse(
+      cli.shell.exec(
+        `gcloud identity groups search --labels="cloudidentity.googleapis.com/groups.${
+          process.env.GROUP_TYPE
+        }" --customer="${process.env.CUSTOMER}" --project="${
+          process.env.PROJECT
+        }" --format=json${
+          nextPageToken ? ` --page-token="${nextPageToken}"` : ''
+        }`
+      ).stdout
+    );
+    groups.push(...page.groups);
+    nextPageToken = page.nextPageToken;
+  } while (nextPageToken);
+  const pattern = new RegExp(
+    permissionsRegex || process.env.PERMISSIONS_REGEX || '.*'
+  );
+
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(groups.filter((group) => pattern.test(group.groupKey.id)))
+  );
+  spinner.succeed(`List saved to ${cli.colors.url(filePath)}`);
+})();
