0cfea0e384ea22df6fd5772771081092667fe505
Seth Battis <sbattis@groton.org>
2024-08-21 20:30:00 -0400

    properly escape regex

--- apps/indexer/src/Helper/subfileFactory.ts
+++ apps/indexer/src/Helper/subfileFactory.ts
@@ -2,7 +2,7 @@ import { Bucket } from '@google-cloud/storage';
 
 export default function subfileFactory(bucket: Bucket) {
   return (uri: string) => {
-    let uriPath = uri.replace(/^gs:\/\/[^/]+\//, '');
+    let uriPath = uri.replace(/^gs:\/\/[^\/]+\//, '');
     return bucket.file(uriPath);
   };
 }
