aa8cbd21813a3d0ae93c5d732c5bcc3ab0a8a495
Seth Battis <sbattis@groton.org>
2024-09-23 10:11:55 -0400

    Fix analytics
    Get it to actually load and collect data!

--- apps/ui/src/DOM/Analytics.ts
+++ apps/ui/src/DOM/Analytics.ts
@@ -11,11 +11,12 @@ export default async function Analytics() {
   await Helper.addScript(
     `https://www.googletagmanager.com/gtag/js?id=${config.googleAnalytics.trackingId}`
   );
-  window.dataLayer = window.dataLayer || [];
-  function gtag(...args: any[]) {
-    window.dataLayer.push(args);
-  }
-  gtag('js', new Date());
-  gtag('config', config.googleAnalytics.trackingId);
+  const s = document.createElement('script');
+  s.innerHTML = `window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '${config.googleAnalytics.trackingId}');`;
+  document.body.append(s);
   Helper.log('Google Analytics loaded');
 }
