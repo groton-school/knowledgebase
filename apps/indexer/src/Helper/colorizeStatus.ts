cff635102b1ebf31f415b3a2cc2419ae25d1a646
Seth Battis <sbattis@groton.org>
2024-09-26 14:14:38 -0400

    Fancy URL formatting
    If I have to keep watching it, it needs to be pretty and pretty consistent too!

--- apps/indexer/src/Helper/colorizeStatus.ts
+++ apps/indexer/src/Helper/colorizeStatus.ts
@@ -1,13 +1,27 @@
-import valid from './validGCSFilenameCharacters';
 import cli from '@battis/qui-cli';
+import path from 'node:path';
+import valid from './validGCSFilenameCharacters';
 
 const GCSPath = new RegExp(`(\/?([${valid}]+\/)+[${valid}]+)`, 'gi');
 
+function fancyUrl(url) {
+  const urlPath = path.dirname(url);
+  return `${urlPath != '.' ? cli.colors.url(`${urlPath}/`) : ''}${cli.colors.value(path.basename(url))}`;
+}
+
 export default function colorizeStatus(status: string) {
-  return status
-    .replace(GCSPath, cli.colors.url('$1'))
-    .replace(
-      /((user|group):[a-z0-9._-]+@[a-z0-9._-]+)/gi,
-      cli.colors.value('$1')
-    );
+  let [, pre = '', url] = status.match(/^(Indexing )(.+)$/) || [];
+  if (url) {
+    status = `${pre}${fancyUrl(url)}`;
+  } else {
+    const [url] = status.match(GCSPath) || [];
+    if (url) {
+      status = status.replace(url, `${fancyUrl(url)}`);
+    }
+  }
+
+  return status.replace(
+    /((user|group):[a-z0-9._-]+@[a-z0-9._-]+)/gi,
+    cli.colors.value('$1')
+  );
 }
