8c35a70db1809f16cf6f6701617e2fbff683f9ad
Seth Battis <sbattis@groton.org>
2024-10-01 20:15:29 -0400

    lose double home

--- apps/ui/src/DOM/BreadCrumbs.ts
+++ apps/ui/src/DOM/BreadCrumbs.ts
@@ -1,7 +1,10 @@
 import path from 'path-browserify';
 
 export default function BreadCrumbs(root: HTMLElement) {
-  const parts = path.dirname(window.location.pathname).split('/');
+  const parts = path
+    .dirname(window.location.pathname)
+    .split('/')
+    .filter((part) => part != '');
   let trail = '/';
   root?.insertAdjacentHTML(
     'afterbegin',
@@ -9,10 +12,11 @@ export default function BreadCrumbs(root: HTMLElement) {
       <div class="card-body">
         <nav aria=label="breadcrumb">
           <ol class="breadcrumb m-0">
+            <li class="breadcrumb-item"><a href="/"><i class="bi bi-house-fill"></i></a></li>
             ${parts
               .map((part: string) => {
                 trail = path.join(trail, part);
-                return `<li class="breadcrumb-item"><a href="${trail}">${part == '' ? '<i class="bi bi-house-fill"></i>' : part}</a></li>`;
+                return `<li class="breadcrumb-item"><a href="${trail}">${part}</a></li>`;
               })
               .join('\n')}
             <li class="breadcrumb-item active">${path.basename(window.location.pathname)}</i>
