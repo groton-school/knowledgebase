fa6bfc2947ae0267468afda4322de9973e642dcd
Seth Battis <sbattis@groton.org>
2025-01-13 15:47:45 -0500

    fix(ui): Fall back to existing document title
    When nothing in the Google Document has been explicitly styled Title, fall back to the existing document title, rather than `undefined`

--- apps/ui/src/DOM/Head.ts
+++ apps/ui/src/DOM/Head.ts
@@ -4,7 +4,7 @@ export default function Head() {
     `
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>${document.querySelector(`.title`)?.textContent}</title>
+        <title>${document.querySelector(`.title`)?.textContent || document.title}</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">`
   );
 }
