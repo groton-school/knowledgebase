e37a604b1bd8836b758a53ff4d91f11ea8217fea
Seth Battis <sbattis@groton.org>
2025-03-03 14:02:58 -0500

    chore(router): bump to gcloud@1.x

--- apps/router/bin/deploy.ts
+++ apps/router/bin/deploy.ts
@@ -1,24 +1,12 @@
-import * as gcloud from '@battis/partly-gcloudy';
+import gcloud from '@battis/partly-gcloudy';
 import cli from '@battis/qui-cli';
 import path from 'node:path';
 
-(async () => {
-  await gcloud.init({
-    env: {
-      root: path.resolve(import.meta.dirname, '../.tmp/isolate'),
-      loadDotEnv: path.resolve(import.meta.dirname, '../../../.env')
-    }
-  });
-  cli.init({
-    env: {
-      root: path.resolve(import.meta.dirname, '../.tmp/isolate'),
-      loadDotEnv: path.resolve(import.meta.dirname, '../../../.env')
-    }
-  });
-  try {
-    await gcloud.batch.appEngineDeployAndCleanup({ retainVersions: 2 });
-    cli.log.info('Deploy complete.');
-  } catch (e) {
-    cli.log.error(e);
-  }
-})();
+console.log(process.env);
+
+cli.configure({
+  root: { root: path.resolve(import.meta.dirname, '../.tmp/isolate') },
+  env: { path: path.resolve(import.meta.dirname, '../../../.env') }
+});
+await cli.init();
+await gcloud.batch.appEngineDeployAndCleanup({ retainVersions: 2 });
